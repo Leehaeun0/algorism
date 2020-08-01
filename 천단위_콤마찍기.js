6649fb4a569fe7667d7813e6c1f29a77b38315da
Leehaeun0 <a01050675111@gmail.com>
2020-08-01 17:25:53 +0900

    study algorism: add comma to number

---
+++ 천단위_콤마찍기.js
@@ -0,0 +1,28 @@
+function commaize1(num) {
+  let returnNum = '';
+  let numToString = num + '';
+
+  const count = Math.ceil(numToString.length / 3);
+
+  for (let i = 1; i <= count; i++) {
+    returnNum = numToString.slice(-3) + ',' + returnNum;
+    numToString = numToString.slice(0, -3);
+  }
+  returnNum = returnNum.slice(0, -1);
+  return returnNum;
+}
+
+function commaize2(num) {
+  let returnNum = '';
+  let numArr = (num + '').split('');
+  numArr.forEach((v, i) => {
+    returnNum = numArr[numArr.length - i - 1] + returnNum;
+    if (!((i % 3) - 2) && i !== numArr.length - 1) returnNum = ',' + returnNum;
+  });
+  console.log(numArr);
+  return returnNum;
+}
+
+console.log(commaize1(1234567));
+console.log(commaize2(1234567));
+console.log(commaize2(1234325333245321367));
