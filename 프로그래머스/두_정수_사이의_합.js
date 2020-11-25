43c387e7f67b1dd29112d8495131f6cbe3887d8f
Leehaeun0 <a01050675111@gmail.com>
2020-11-25 21:54:06 +0900

    study algorism

---
+++ 프로그래머스/두_정수_사이의_합.js
@@ -0,0 +1,15 @@
+const sum = (more, less) => {
+  const arr = [];
+  for (let i = less; i <= more; i++) {
+    arr.push(i);
+  }
+  return arr.reduce((acc, cur) => acc + cur, 0);
+};
+
+function solution(a, b) {
+  if (a === b) return a;
+  if (a > b) return sum(a, b);
+  return sum(b, a);
+}
+
+console.log(solution(3, 5)); // 12
